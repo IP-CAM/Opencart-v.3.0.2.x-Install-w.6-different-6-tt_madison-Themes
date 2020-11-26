94c655ea9f6110a9540686ee800ff6f7c94518f5
huỳnh tân <lyhuynhtan2206@gmail.com>
2020-11-26 10:39:19 +0700

    sua startup index

--- system/startup.php
+++ system/startup.php
@@ -106,9 +106,16 @@ require_once(modification('./system/engine/registry.php'));
 require_once(modification('./system/engine/proxy.php'));
 
 // Helper
-require_once(DIR_SYSTEM . 'helper/general.php');
-require_once(DIR_SYSTEM . 'helper/utf8.php');
+// require_once(DIR_SYSTEM . 'helper/general.php');
+// require_once(DIR_SYSTEM . 'helper/utf8.php');
+
+// function start($application_config) {
+// 	require_once(DIR_SYSTEM . 'framework.php');	
+// }
+
+require_once('./system/helper/general.php');
+require_once('./system/helper/utf8.php');
 
 function start($application_config) {
-	require_once(DIR_SYSTEM . 'framework.php');	
+	require_once('./system/framework.php');	
 }
