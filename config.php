e7329c1d37a2e253929104fee22fc7ba25f4aadf
huỳnh tân <lyhuynhtan2206@gmail.com>
2020-11-26 10:15:37 +0700

    sua confgig

--- config.php
+++ config.php
@@ -1,18 +1,31 @@
 <?php
+// // HTTP
+// define('HTTP_SERVER', 'http://localhost/ShopHoa/tt_madison_quickstart/');
+// //sua
+// // HTTPS
+// define('HTTPS_SERVER', 'http://localhost/ShopHoa/tt_madison_quickstart/');
+// //sua
+// // DIR
+// define('DIR_APPLICATION', 'C:/xampp/htdocs/ShopHoa/tt_madison_quickstart/catalog/');
+
+// define('DIR_SYSTEM', 'C:/xampp/htdocs/ShopHoa/tt_madison_quickstart/system/');
+// define('DIR_IMAGE', 'C:/xampp/htdocs/ShopHoa/tt_madison_quickstart/image/');
+//sua
+
+
 // HTTP
-define('HTTP_SERVER', 'http://localhost/ShopHoa/tt_madison_quickstart/');
+define('HTTP_SERVER', 'https://bffkantho.herokuapp.com/');
 //sua
 // HTTPS
-define('HTTPS_SERVER', 'http://localhost/ShopHoa/tt_madison_quickstart/');
+define('HTTPS_SERVER', 'https://bffkantho.herokuapp.com/');
 //sua
 // DIR
-define('DIR_APPLICATION', 'C:/xampp/htdocs/ShopHoa/tt_madison_quickstart/catalog/');
+define('DIR_APPLICATION', 'https://bffkantho.herokuapp.com/catalog/');
 
-define('DIR_SYSTEM', 'C:/xampp/htdocs/ShopHoa/tt_madison_quickstart/system/');
-define('DIR_IMAGE', 'C:/xampp/htdocs/ShopHoa/tt_madison_quickstart/image/');
-//sua
+define('DIR_SYSTEM', 'https://bffkantho.herokuapp.com/system/');
+define('DIR_IMAGE', 'https://bffkantho.herokuapp.com/image/');
 
-define('DIR_STORAGE', 'C:/xampp/htdocs/ShopHoa/tt_madison_quickstart/storage/');
+define('DIR_STORAGE', 'https://bffkantho.herokuapp.com/storage/');
 define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
 define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
 define('DIR_CONFIG', DIR_SYSTEM . 'config/');
